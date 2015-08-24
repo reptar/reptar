a63aceb357928f181eaba83f9e06d6e1b1995dd2
Harry Wolff <hswolff@gmail.com>
2015-08-23 20:24:25 -0400

    Add code that handles the asset pipeline. Built-in less and browserify for now.

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require('winston').cli({
   colorize: true
 });
 
+const logger = require('winston');
 const Yarn = require('./lib/index');
 
-module.exports = function(done) {
+module.exports = async function(done) {
+  logger.profile('yarn');
+
   let yarn = new Yarn();
   yarn.readFiles();
-  yarn.writeFiles();
+  await yarn.writeFiles();
+
+  logger.profile('yarn');
   done();
 };
