706c235a474b6a1d15ae5f97edc7363838cd3670
Harry Wolff <hswolff@gmail.com>
2015-08-29 21:09:54 -0400

    Move Plugin to be a class that's a singleton.
    Also fix up the processEventHandlers() to prevent bad plugin actors.

--- test/unit/plugin/index.spec.js
+++ test/unit/plugin/index.spec.js
@@ -48,8 +48,8 @@ describe('plugin/index Plugin', function() {
     });
 
     it('can invoke handlers sequentionally', async (done) => {
-      let passThroughFunc = sinon.spy((val) => {
-        return val;
+      let noopFunc = sinon.spy(() => {
+        // noop
       });
 
       let promiseFn = sinon.spy((val) => {
@@ -58,17 +58,23 @@ describe('plugin/index Plugin', function() {
         });
       });
 
-      Plugin.addEventHandler(handlerName, passThroughFunc);
+      Plugin.addEventHandler(handlerName, noopFunc);
       Plugin.addEventHandler(handlerName, promiseFn);
 
       assert.isArray(Plugin._handlers[handlerName]);
       assert.lengthOf(Plugin._handlers[handlerName], 2);
 
       let handlerValue = 5;
-      let newVal = await Plugin.processEventHandlers(handlerName, handlerValue);
+      let newVal;
 
-      assert.equal(passThroughFunc.callCount, 1);
-      assert.ok(passThroughFunc.calledWith(5));
+      try {
+        newVal = await Plugin.processEventHandlers(handlerName, handlerValue);
+      } catch (e) {
+        console.log(e);
+      }
+
+      assert.equal(noopFunc.callCount, 1);
+      assert.ok(noopFunc.calledWith(5));
 
       assert.equal(promiseFn.callCount, 1);
       assert.ok(promiseFn.calledWith(5));
@@ -78,8 +84,10 @@ describe('plugin/index Plugin', function() {
       let modifierFn = sinon.spy((val) => {
         return val * val;
       });
+      let trailingNoopFn = sinon.spy();
 
       Plugin.addEventHandler(handlerName, modifierFn);
+      Plugin.addEventHandler(handlerName, trailingNoopFn);
 
       try {
         newVal = await Plugin.processEventHandlers(handlerName, handlerValue);
@@ -87,10 +95,11 @@ describe('plugin/index Plugin', function() {
         throw e;
       }
 
-      assert.equal(passThroughFunc.callCount, 2);
+      assert.equal(noopFunc.callCount, 2);
       assert.equal(promiseFn.callCount, 2);
       assert.equal(modifierFn.callCount, 1);
       assert.ok(modifierFn.calledWith(5));
+      assert.equal(trailingNoopFn.callCount, 1);
 
       assert.equal(newVal, handlerValue * handlerValue);
 
