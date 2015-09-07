604cb8e7ea22b7c5032aef9bab4bbaa4e3e3e1d8
Harry Wolff <hswolff@gmail.com>
2015-09-07 11:32:26 -0400

    Allow Yarn to be instantiated with a defined cwd.

--- test/unit/theme/index.spec.js
+++ test/unit/theme/index.spec.js
@@ -1,22 +1,10 @@
-const assert = require('assert');
+// const assert = require('assert');
 
-const Theme = require('../../../lib/theme/index');
+// const Theme = require('../../../lib/theme/index.js');
 
-describe.skip('Theme', function() {
-  describe('new', function() {
-    it('should scaffold a new blog', async function() {
-      assert.equal(5, 5, 'They did it.');
-      let theme = new Theme();
-      theme.update();
-      console.log('test');
-      try {
-        await theme.write();
-      } catch (e) {
-        console.log(e.stack);
-      } finally {
-
-      }
-      console.log('again');
+describe('Theme', function() {
+  describe('TODO', function() {
+    it('TODO', function() {
     });
   });
 });
