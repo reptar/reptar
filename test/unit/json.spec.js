c5e0e8fe24a7debb98a62107b1a430c755ef19b0
Harry Wolff <hswolff@gmail.com>
2015-10-12 17:15:00 -0400

    Create json module to handle json related operations.

---
+++ test/unit/json.spec.js
@@ -0,0 +1,18 @@
+import {assert} from 'chai';
+import path from 'path';
+import * as json from '../../lib/json.js';
+
+describe('json JSON', function() {
+
+  describe('getYarnPackageNames', () => {
+    it('returns the yarn package names', () => {
+      let rootDir = path.resolve(__dirname, '../../');
+      let plugins = json.getYarnPackageNames(rootDir);
+
+      assert.sameMembers(
+        Array.from(plugins),
+        ['yarn-excerpt', 'yarn-html-minifier']
+      );
+    });
+  });
+});
