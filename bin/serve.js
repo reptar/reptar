ff98933374d26a70f3c9420cc9b657003ecff2d4
Harry Wolff <hswolff@gmail.com>
2016-01-19 00:00:05 -0500

    Fix yarn serve so that it loads the right config.yml file.
    Also have it use settings created in the config.yml file.

--- bin/serve.js
+++ bin/serve.js
@@ -2,13 +2,16 @@ var child_process = require('child_process');
 
 module.exports = function() {
   var config = require('../lib/config');
+
+  config.setRoot(config.findLocalDir());
+
   var destination = config.path.destination || './_site';
 
   child_process.spawn('http-server', [
-    destination, // Path to serve from.
-    '-p 8080',   // Use port 8080.
-    '-d',        // Show directory listings.
-    '-c-1'       // Disable caching.
+    destination,
+    '-p ' + (config.server.port || 8080),
+    '-d',
+    '-c-1'
   ], {
     stdio: 'inherit'
   });
