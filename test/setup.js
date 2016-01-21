0ba1876329a87c82c3c4439058e98dc08e69ad94
Harry Wolff <hswolff@gmail.com>
2016-01-21 09:55:45 -0500

    Add support for travis and coveralls.

---
+++ test/setup.js
@@ -0,0 +1,14 @@
+require('babel-polyfill');
+require('babel-register');
+require('mocha-clean');
+
+// For now we need to ensure that our local defaults.yml file is loaded into
+// our config singleton to make tests behave as expected.
+// @TODO: don't require any of the following code.
+const config = require('../lib/config');
+const path = require('path');
+const pathToScaffold = path.resolve(
+  __dirname,
+  '../node_modules/yarn-scaffold'
+);
+config.setRoot(pathToScaffold);
