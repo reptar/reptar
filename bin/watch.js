492a77308275cb6a1d66f040009c18c8466b7ff0
Harry Wolff <hswolff@gmail.com>
2016-10-16 16:03:12 -0400

    feat(watch): Use web server to implement more durable watch mode.
    Closes #43. This uses Hapi to create a web server that renders each
    File or CollectionPage on the fly so it always reflects the latest
    content from the file system.

--- bin/watch.js
+++ bin/watch.js
@@ -1,113 +1,237 @@
-import Promise from 'bluebird';
 import activity from 'activity-logger';
 import _ from 'lodash';
-import log from '../lib/log';
-import { YAML } from '../lib/constants';
-import Reptar from '../lib';
+import path from 'path';
+import Hapi from 'hapi';
+import inert from 'inert';
 import chokidar from 'chokidar';
-import serve from './serve';
+import { YAML } from '../lib/constants';
+import log from '../lib/log';
+import Reptar from '../lib';
 
-export default function() {
-  log.setSilent(true);
-  serve({
-    showOutput: false
-  });
+class Server {
+  constructor(reptar) {
+    this.reptar = reptar;
 
-  const startActivity = activity.start('Starting watching files.');
+    /**
+     * Index used to find associated File/CollectionPage with a request.path.
+     * @type {Object.<string,(File|CollectionPage)>}
+     */
+    this.index = {};
 
-  const reptar = new Reptar({
-    // Turn on incremental building.
-    incremental: true,
+    this.server = new Hapi.Server();
 
-    // Turn off caching of templates.
-    noTemplateCache: true
-  });
-
-  reptar.update()
-    .catch(function(e) {
-      log.error(e.stack);
-      throw e;
+    this.server.connection({
+      host: reptar.config.get('server.host'),
+      port: reptar.config.get('server.port'),
     });
-
-  const configPath = reptar.getConfig().get('path');
-
-  const watcher = chokidar.watch([
-    configPath.source
-  ], {
-    atomic: true,
-  });
-
-  function shouldIgnorePath(path) {
-    return [
-      '/.git/', // ignore .git directory.
-      configPath.plugins,
-      configPath.themes,
-      configPath.destination
-    ].some(configPath => path.indexOf(configPath) > -1);
   }
 
-  // Wait for watcher to be ready before registering other watchers.
-  watcher.on('ready', function() {
-    activity.end(startActivity);
+  /**
+   * Starts the Hapi server.
+   * @return {Promise}
+   */
+  async start() {
+    await this.server.register([
+      inert,
+    ]);
 
-    console.log('');
-    log.info('Ready...\n');
+    this.server.route({
+      method: 'GET',
+      path: '/{p*}',
+      handler: this.routeHandler,
+    });
 
-    function handleFsChange(event, path) {
-      if (shouldIgnorePath(path)) {
-        return;
+    this.updateIndex();
+
+    this.createFsWatchers();
+
+    // Start the server
+    return this.server.start();
+  }
+
+  relativeDestination(destination) {
+    return destination.replace(
+      this.reptar.config.get('path.destination'),
+      ''
+    );
+  }
+
+  /**
+   * Update's our index.
+   */
+  updateIndex() {
+    const createIndexKey = (file) => {
+      return this.relativeDestination(file.destination);
+    };
+
+    this.index = _.extend(
+      _.keyBy(this.reptar.files, createIndexKey),
+      ..._.map(this.reptar.collections, collection =>
+        _.keyBy(collection.pages, createIndexKey)
+      )
+    );
+  }
+
+  /**
+   * Get File/CollectionPage based on request.path.
+   * @param {string} requestPath Request path to server.
+   * @return {File|CollectionPage}
+   */
+  getFile(requestPath) {
+    let file = this.index[requestPath];
+    if (!file) {
+      file = this.index[path.join(requestPath, 'index.html')];
+    }
+    return file;
+  }
+
+  /**
+   * Our default route handler for every request.
+   * @param {Object} request Hapi Request object.
+   * @param {Object} reply Hapi Response object.
+   * @return {Promise}
+   */
+  routeHandler = async (request, reply) => {
+    const isDebug = request.query.debug != null;
+
+    let file = this.getFile(request.path);
+
+    // If there's no associated File/CollectionPage then we pass handling to our
+    // themeHandler.
+    if (!file) {
+      const response = this.themeHandler(request, reply);
+
+      // If we found an asset response then just return it.
+      if (response != null) {
+        return response;
       }
 
-      log.info('Change detected at: ' + path);
+      // However if we did not find a File/CollectionPage or an asset response
+      // then we're going to update our Reptar instance and attempt to find
+      // a file again.
+      await this.reptar.update();
+      this.updateIndex();
 
-      const id = activity.start('Rebuilding...');
+      file = this.getFile(request.path);
 
-      let promise;
-      // If `_config.yml` changed then re-load from fs.
-      if (path.indexOf(YAML.CONFIG) > -1) {
-        promise = reptar.update();
-      } else {
-        promise = Promise.resolve();
+      // If we still don't have a File/CollectionPage then return 404.
+      if (!file) {
+        return reply('404');
       }
-
-      promise.then(function() {
-        return reptar.readFiles(path);
-      }).then(function() {
-        return reptar.build();
-      }).then(() => {
-        activity.end(id);
-
-        const date = new Date();
-        log.info('\t\tdone! (' + date.toISOString() + ')');
-        console.log('');
-      });
     }
 
-    // Watch for all fs events.
-    watcher.on('all', _.debounce(handleFsChange, 200));
-  });
+    // If this File does not require any processing then it's a static asset
+    // and we can just render it.
+    if (file.skipProcessing) {
+      return reply.file(file.path);
+    }
 
-  // Handle when theme files change and re-build entire source to reflect new
-  // theme changes.
-  const themeWatcher = chokidar.watch([
-    configPath.themes
-  ]);
-  themeWatcher.on('ready', function() {
+    // Update the File/CollectionPage from disk.
+    await file.update(this.reptar.data);
 
-    themeWatcher.on('change', function(path) {
-      log.info('Theme file changed at: ' + path);
-      const id = activity.start('Rebuilding...');
+    // Depending on what Files were updated we should update our Collections.
+    this.reptar.readCollections();
 
-      reptar.update()
-        .then(function() {
-          return reptar.build();
-        }).then(function() {
-          activity.end(id);
+    // Render the File/CollectionPage.
+    const content = await file.renderWithPlugins(this.reptar.data);
 
-          const date = new Date();
-          log.info('\t\tdone! (' + date.toISOString() + ')');
-          console.log('');
-        });
+    // If we want debug information then render the JSON version.
+    if (isDebug) {
+      return reply(JSON.stringify(file)).type('application/json');
+    }
+
+    if (file.path != null) {
+      log.info(`Rendering File ${file.id}`);
+    } else {
+      log.info(`Rendering CollectionPage ${file.id}`);
+    }
+
+    return reply(content);
+  }
+
+  /**
+   * Handler to use for theme assets.
+   * @param {Object} request Hapi Request object.
+   * @param {Object} reply Hapi Response object.
+   * @return {Object?} Optional response object, null if we can't find any
+   *   associated asset.
+   */
+  themeHandler(request, reply) {
+    const requestPath = request.path;
+
+    // Find an associated theme asset.
+    const asset = _.find(this.reptar.theme.assets, asset => {
+      const destination = this.relativeDestination(
+        asset.processor && asset.destination ?
+          asset.destination :
+          asset.config.destination
+      );
+
+      return requestPath.includes(destination);
     });
+
+    if (asset == null) {
+      return;
+    }
+
+    if (asset.processor != null && asset.content) {
+      const contentType = request.server.mime.path(request.path).type;
+      return reply(asset.content).type(contentType);
+    }
+
+    const relativeRequestPath = requestPath.replace(
+      this.relativeDestination(asset.config.destination),
+      ''
+    );
+
+    return reply.file(
+      path.join(asset.config.source, relativeRequestPath)
+    );
+  }
+
+  /**
+   * Create file system watchers to update Reptar state according to when a
+   * user updates files.
+   */
+  createFsWatchers() {
+    chokidar.watch([
+      this.reptar.theme.config.path.source,
+    ]).on('change', async (changePath) => {
+      log.info(`Theme updated at ${changePath}`);
+
+      const id = activity.start('Updating theme.');
+      await this.reptar.theme.read();
+      this.updateIndex();
+      activity.end(id);
+    });
+
+    chokidar.watch([
+      path.join(this.reptar.config.root, YAML.CONFIG)
+    ]).on('change', async (changePath) => {
+      log.info(`_config.yml updated at ${changePath}`);
+
+      await this.reptar.update();
+      this.updateIndex();
+    });
+  }
+}
+
+export default async function watch(options = {}) {
+  const startActivity = activity.start('Starting watch.');
+
+  const reptar = new Reptar({
+    // Turn off caching of templates.
+    noTemplateCache: true,
+
+    ...options,
   });
+
+  await reptar.update();
+
+  const server = new Server(reptar);
+  await server.start();
+
+  activity.end(startActivity);
+
+  log.info('Server running at:', server.server.info.uri);
 }
