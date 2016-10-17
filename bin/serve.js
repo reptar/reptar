b238130a64c64a72fba2a882f8f231a1298ddd09
Harry Wolff <hswolff@gmail.com>
2016-10-16 20:31:40 -0400

    feat(serve): Just use Hapi as our web server.
    This removes live-server dependency.

--- bin/serve.js
+++ bin/serve.js
@@ -1,14 +1,44 @@
-import liveServer from 'live-server';
+import Hapi from 'hapi';
+import inert from 'inert';
 import Config from '../lib/config';
+import log from '../lib/log';
 
-export default function() {
+export default async function serve() {
   const config = new Config();
   config.update();
 
-  liveServer.start({
-    port: config.get('server.port'),
-    root: config.get('path.destination'),
-    open: false,
-    logLevel: 2,
+  const server = new Hapi.Server({
+    connections: {
+      routes: {
+        files: {
+          relativeTo: config.get('path.destination'),
+        },
+      },
+    },
   });
+
+  server.connection({
+    host: config.get('server.host'),
+    port: config.get('server.port'),
+  });
+
+  await server.register([
+    inert,
+  ]);
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        redirectToSlash: true,
+        index: true
+      },
+    }
+  });
+
+  server.start();
+
+  log.info('Server running at:', server.info.uri);
 }
