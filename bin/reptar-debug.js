482e2b3ccd6da356f4ae31d658106db72d90fd14
Harry Wolff <hswolff@gmail.com>
2017-03-25 11:31:24 -0400

    chore: Move all cli scripts into lib folder.

--- bin/reptar-debug.js
+++ bin/reptar-debug.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+/* eslint-disable global-require */
 
 require('babel-polyfill');
 require('babel-register');
 
-require('./index');
+const path = require('path');
+
+require('./index')({
+  log: require('../lib/log').default,
+  libPath: path.join(__dirname, '..', 'lib'),
+});
